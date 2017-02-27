501d77c7814564b1bd934dd3e028ed6a603473b3
Pauline Tarriol <pauline.tarriol@gmail.com>
2017-02-28 00:36:36 +0100

    Update

--- Test.py
+++ Test.py
@@ -6,10 +6,9 @@ def bond(amount, rate, maturity):
     return price_bond
 
 for i in range(1, 101):
-    list[i] = (bond(10000, 0.03, i))
+    (bond(10000, 0.03, i))
 
     plt.plot(range, i)
     plt.ylabel('Price Bond')
     plt.xlabel('Maturity')
     plt.show()
-
