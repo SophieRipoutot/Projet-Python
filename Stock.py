29e3dfb725cbbecf3dbab95a833c8981461694dd
Pauline Tarriol <pauline.tarriol@gmail.com>
2017-02-28 16:20:45 +0100

    investors

--- Stock.py
+++ Stock.py
@@ -37,3 +37,39 @@ class Stock (object):
         self.amount = amount
         self.number = number
         self.date = date
+
+
+row_count = sum(1 for row in AAPL)
+print(row_count)
+
+row_count = 2547
+
+
+#def price():
+#    list_stock_price = []
+ #   for i in range(1, row_count):
+ #       list_stock_price.append(AAPLhigh(i))
+
+
+class Buy(Stock):
+    def __init__(self, name, term, amount, number, date, bs):
+        list_bs = ['buy', 'sell']
+        super(STBond, self).__init__(name, term, amount, number, date)
+        self.bs = list_bs[0]
+        self.name = name
+        self.term = term
+        self.amount = amount
+        self.number = number
+        self.date = date
+
+
+class Sell (Stock):
+    def __init__(self, name, term, amount, number, date, bs):
+        list_bs = ['buy', 'sell']
+        super(STBond, self).__init__(name, term, amount, number, date)
+        self.bs = list_bs = [1]
+        self.name = name
+        self.term = term
+        self.amount = amount
+        self.number = number
+        self.date = date
